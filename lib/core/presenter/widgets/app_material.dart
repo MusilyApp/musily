6100c955c7bf700114d42f63d76f88ddf5031fe7
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-08-17 01:51:38 -0300

    Added: Settings Controller to manage language and themes

--- lib/core/presenter/widgets/app_material.dart
+++ lib/core/presenter/widgets/app_material.dart
@@ -2,6 +2,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:musily/features/settings/presenter/controllers/settings/settings_controller.dart';
 
 class AppMaterial extends StatelessWidget {
   const AppMaterial({super.key});
@@ -9,36 +10,43 @@ class AppMaterial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Modular.setInitialRoute('/sections/');
-    return MaterialApp.router(
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-        ),
-        useMaterial3: true,
-        cardTheme: const CardTheme(
-          shadowColor: Colors.transparent,
-        ),
-      ),
-      darkTheme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.dark,
-        ),
-        scaffoldBackgroundColor: Colors.black,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.black,
-        ),
-        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-          backgroundColor: Colors.black,
-        ),
-        cardTheme: const CardTheme(
-          color: Color.fromARGB(255, 10, 10, 10),
-        ),
-      ),
-      routerConfig: Modular.routerConfig,
+    final settingsController = Modular.get<SettingsController>();
+    return settingsController.builder(
+      builder: (context, data) {
+        return MaterialApp.router(
+          locale: data.locale,
+          themeMode: data.themeMode,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.deepPurple,
+            ),
+            useMaterial3: true,
+            cardTheme: const CardTheme(
+              shadowColor: Colors.transparent,
+            ),
+          ),
+          darkTheme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: Colors.deepPurple,
+              brightness: Brightness.dark,
+            ),
+            scaffoldBackgroundColor: Colors.black,
+            appBarTheme: const AppBarTheme(
+              backgroundColor: Colors.black,
+            ),
+            bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+              backgroundColor: Colors.black,
+            ),
+            cardTheme: const CardTheme(
+              color: Color.fromARGB(255, 10, 10, 10),
+            ),
+          ),
+          routerConfig: Modular.routerConfig,
+        );
+      },
     );
   }
 }
