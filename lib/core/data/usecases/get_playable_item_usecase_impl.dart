0c1457de0700296ef6736d69e9f807c2e4d1e41f
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-08-17 01:44:40 -0300

    Changed: Search source to MusilyRepository

--- lib/core/data/usecases/get_playable_item_usecase_impl.dart
+++ lib/core/data/usecases/get_playable_item_usecase_impl.dart
@@ -2,11 +2,13 @@ import 'package:musily/core/domain/errors/app_error.dart';
 import 'package:musily/core/domain/uasecases/get_playable_item_usecase.dart';
 import 'package:musily/core/utils/string_is_url.dart';
 import 'package:musily_player/musily_entities.dart';
+import 'package:musily_repository/musily_repository.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 class GetPlayableItemUsecaseImpl implements GetPlayableItemUsecase {
   @override
   Future<MusilyTrack> exec(MusilyTrack track) async {
+    final musilyRepository = MusilyRepository();
     final yt = YoutubeExplode();
     late final String ytId;
 
@@ -14,7 +16,7 @@ class GetPlayableItemUsecaseImpl implements GetPlayableItemUsecase {
     if (track.ytId == track.id) {
       ytId = track.id;
     } else {
-      final searchResults = await yt.search.search(
+      final searchResults = await musilyRepository.searchTracks(
         '${track.title} ${track.artist?.name}',
       );
       if (searchResults.isEmpty) {
