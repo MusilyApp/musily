51bf74194a51a7865189a53fb73b26b9cf215780
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-08-17 01:54:32 -0300

    Added: User Avatar widget

---
+++ lib/core/presenter/widgets/user_avatar.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:musily/core/presenter/controllers/core/core_controller.dart';
+import 'package:musily/core/utils/get_theme_mode.dart';
+import 'package:musily/features/settings/presenter/controllers/settings/settings_controller.dart';
+import 'package:musily/features/settings/presenter/pages/settings_page.dart';
+
+class UserAvatar extends StatelessWidget {
+  final CoreController coreController;
+  final SettingsController settingsController;
+  const UserAvatar({
+    super.key,
+    required this.coreController,
+    required this.settingsController,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    bool isDarkMode = getThemeMode(context) == ThemeMode.dark;
+    return InkWell(
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      hoverColor: Colors.transparent,
+      onTap: () {
+        coreController.methods.pushWidget(
+          SettingsPage(
+            coreController: coreController,
+            settingsController: settingsController,
+          ),
+        );
+      },
+      child: CircleAvatar(
+        backgroundColor:
+            isDarkMode ? const Color(0xffe8def7) : const Color(0xff68548f),
+        child: Icon(
+          Icons.person_rounded,
+          color:
+              !isDarkMode ? const Color(0xffe8def7) : const Color(0xff68548f),
+        ),
+      ),
+    );
+  }
+}
