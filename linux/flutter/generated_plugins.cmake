74295b0a3653c1d2ff0079adf39c3ad84459d39d
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-10-11 11:55:42 -0300

    updated: dependencies

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -3,12 +3,11 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
-  flutter_secure_storage_linux
+  flutter_secure_storage
   gtk
   isar_flutter_libs
   media_kit_libs_linux
   url_launcher_linux
-  window_to_front
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
