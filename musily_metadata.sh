4ae02d9e3553fdddb0567153816a802100dee932
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-10-11 12:12:09 -0300

    fix: scaping special characters in description

--- musily_metadata.sh
+++ musily_metadata.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# Caminho do arquivo pubspec.yaml e CHANGELOG.md
+# Path to the pubspec.yaml and CHANGELOG.md files
 pubspec_file="pubspec.yaml"
 changelog_file="CHANGELOG.md"
 
-# Função para extrair a versão do pubspec.yaml
+# Function to extract the version from pubspec.yaml
 get_version() {
   version=$(grep 'version:' "$pubspec_file" | awk '{print $2}' | awk -F'+' '{print $1}')
   echo "$version"
 }
 
-# Função para extrair a descrição do CHANGELOG.md com base na versão
+# Function to extract the description from CHANGELOG.md based on the version
 get_description() {
   version=$1
   description=$(awk '/^##[[:blank:]]*'"${version}"'[[:blank:]]*$/ { flag=1; next } flag && /^##/ { flag=0 } flag { buffer=buffer $0 "\n" } END { print buffer }' "$changelog_file")
 
-  # Verifica se a descrição foi encontrada
+  # Check if the description was found
   if [ -z "$description" ]; then
     echo "Description not found in $changelog_file for version $version"
     exit 1
@@ -24,14 +24,14 @@ get_description() {
   echo "$description"
 }
 
-# Função para exibir ajuda
+# Function to display help
 show_help() {
   echo "Usage: $0 [--version | --description]"
   echo "  --version       Display the current version from pubspec.yaml"
   echo "  --description   Display the description for the current version from CHANGELOG.md"
 }
 
-# Verifica os argumentos passados
+# Check the passed arguments
 case "$1" in
   --version)
     get_version
