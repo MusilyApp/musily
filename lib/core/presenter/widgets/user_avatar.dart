bccd8136264a09ddcef678458911474e027b1e72
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-11-03 14:53:37 -0300

    improved: navigation

--- lib/core/presenter/widgets/user_avatar.dart
+++ lib/core/presenter/widgets/user_avatar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:musily/core/presenter/extensions/build_context.dart';
 import 'package:musily/core/presenter/controllers/core/core_controller.dart';
-import 'package:musily/core/utils/display_helper.dart';
-import 'package:musily/core/utils/get_theme_mode.dart';
+import 'package:musily/core/presenter/ui/utils/ly_navigator.dart';
 import 'package:musily/features/auth/presenter/controllers/auth_controller/auth_controller.dart';
 import 'package:musily/features/settings/presenter/controllers/settings/settings_controller.dart';
 import 'package:musily/features/settings/presenter/pages/settings_page.dart';
@@ -19,32 +19,41 @@ class UserAvatar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    bool isDarkMode = getThemeMode(context) == ThemeMode.dark;
     return InkWell(
       splashColor: Colors.transparent,
       highlightColor: Colors.transparent,
       hoverColor: Colors.transparent,
       onTap: () {
-        coreController.methods.pushWidget(
-          SettingsPage(
-            coreController: coreController,
-            settingsController: settingsController,
-            authController: authController,
-          ),
-          overlayMainPage: !DisplayHelper(context).isDesktop,
-        );
+        late final BuildContext? usingContext;
+
+        if (context.display.isDesktop) {
+          usingContext = context;
+        } else {
+          usingContext = coreController.coreKey.currentContext;
+        }
+        if (usingContext != null) {
+          LyNavigator.push(
+            usingContext,
+            SettingsPage(
+              coreController: coreController,
+              settingsController: settingsController,
+              authController: authController,
+            ),
+          );
+        }
       },
       child: CircleAvatar(
         radius: 19,
-        backgroundColor:
-            isDarkMode ? const Color(0xffe8def7) : const Color(0xff68548f),
+        backgroundColor: context.isDarkMode
+            ? const Color(0xffe8def7)
+            : const Color(0xff68548f),
         child: authController.builder(builder: (context, data) {
           if (data.user != null) {
             return Text(
               data.user!.name[0],
               style: TextStyle(
                 fontWeight: FontWeight.bold,
-                color: !isDarkMode
+                color: !context.isDarkMode
                     ? const Color(0xffe8def7)
                     : const Color(0xff68548f),
               ),
@@ -52,8 +61,9 @@ class UserAvatar extends StatelessWidget {
           }
           return Icon(
             Icons.person_rounded,
-            color:
-                !isDarkMode ? const Color(0xffe8def7) : const Color(0xff68548f),
+            color: !context.isDarkMode
+                ? const Color(0xffe8def7)
+                : const Color(0xff68548f),
           );
         }),
       ),
