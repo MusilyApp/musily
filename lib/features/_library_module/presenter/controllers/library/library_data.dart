4a2e9fe67465a34b909cd5d4af6f1b91b991848b
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-11-03 14:55:34 -0300

    feature: cloud library compatibility

--- lib/features/_library_module/presenter/controllers/library/library_data.dart
+++ lib/features/_library_module/presenter/controllers/library/library_data.dart
@@ -3,35 +3,36 @@ import 'package:musily/features/_library_module/domain/entities/library_item_ent
 
 class LibraryData extends BaseControllerData {
   final bool loading;
-  final bool addingToFavorites;
+  final List<String> itemsAddingToFavorites;
   final List<LibraryItemEntity> items;
-  final bool addingToLibrary;
+  final List<String> itemsAddingToLibrary;
   final List<String> loadedFavoritesHash;
   final List<String> alreadyLoadedFirstFavoriteState;
   LibraryData({
     required this.loading,
     required this.items,
-    required this.addingToLibrary,
+    required this.itemsAddingToLibrary,
     required this.loadedFavoritesHash,
     required this.alreadyLoadedFirstFavoriteState,
-    required this.addingToFavorites,
+    required this.itemsAddingToFavorites,
   });
 
   @override
   LibraryData copyWith({
     bool? loading,
     List<LibraryItemEntity>? items,
-    bool? addingToLibrary,
+    List<String>? itemsAddingToLibrary,
     List<String>? loadedFavoritesHash,
     List<String>? alreadyLoadedFirstFavoriteState,
-    bool? addingToFavorites,
+    List<String>? itemsAddingToFavorites,
   }) {
     return LibraryData(
       loading: loading ?? this.loading,
       items: items ?? this.items,
-      addingToLibrary: addingToLibrary ?? this.addingToLibrary,
+      itemsAddingToLibrary: itemsAddingToLibrary ?? this.itemsAddingToLibrary,
       loadedFavoritesHash: loadedFavoritesHash ?? this.loadedFavoritesHash,
-      addingToFavorites: addingToFavorites ?? this.addingToFavorites,
+      itemsAddingToFavorites:
+          itemsAddingToFavorites ?? this.itemsAddingToFavorites,
       alreadyLoadedFirstFavoriteState: alreadyLoadedFirstFavoriteState ??
           this.alreadyLoadedFirstFavoriteState,
     );
--- lib/features/_library_module/presenter/controllers/library/library_methods.dart
+++ lib/features/_library_module/presenter/controllers/library/library_methods.dart
@@ -1,28 +1,48 @@
+import 'package:musily/features/_library_module/data/dtos/create_playlist_dto.dart';
+import 'package:musily/features/_library_module/data/dtos/update_playlist_dto.dart';
 import 'package:musily/features/_library_module/domain/entities/library_item_entity.dart';
+import 'package:musily/features/album/domain/entities/album_entity.dart';
+import 'package:musily/features/artist/domain/entitites/artist_entity.dart';
 import 'package:musily/features/track/domain/entities/track_entity.dart';
 
 class LibraryMethods {
-  final Future<void> Function<T>(T item) addToLibrary;
-  final Future<void> Function() getLibrary;
+  // Library
+  final Future<void> Function({bool showLoading}) getLibraryItems;
+  final Future<LibraryItemEntity?> Function(String itemId) getLibraryItem;
+
+  // Playlist
+  final Future<void> Function(CreatePlaylistDTO data) createPlaylist;
+
   final Future<void> Function(
+    String playlistId,
     List<TrackEntity> tracks,
-    String playlistId,
-  ) addToPlaylist;
+  ) addTracksToPlaylist;
+
   final Future<void> Function(
     String playlistId,
-    TrackEntity track,
-  ) removeFromPlaylist;
-  final Future<void> Function(String id) deleteLibraryItem;
+    List<String> tracksIds,
+  ) removeTracksFromPlaylist;
+
   final Future<void> Function(
-    TrackEntity track, {
-    bool ignoreIfAdded,
-  }) toggleFavorite;
+    UpdatePlaylistDto data,
+  ) updatePlaylist;
+
+  final Future<void> Function(
+    String playlistId,
+  ) removePlaylistFromLibrary;
+
+  // Artist
+  final Future<void> Function(ArtistEntity artist) addArtistToLibrary;
+
+  final Future<void> Function(String artistId) removeArtistFromLibrary;
+
+  // Album
+  final Future<void> Function(AlbumEntity album) addAlbumToLibrary;
+
+  final Future<void> Function(String albumId) removeAlbumFromLibrary;
+
   bool Function(TrackEntity track) isFavorite;
   Future<void> Function() loadFavorites;
-  Future<void> Function(
-    String id,
-    String name,
-  ) updatePlaylistName;
 
   final Future<void> Function(String id) updateLastTimePlayed;
 
@@ -30,25 +50,28 @@ class LibraryMethods {
     List<TrackEntity> tracks,
     String downloadingId,
   ) downloadCollection;
+
   Future<void> Function(
     List<TrackEntity> tracks,
     String downloadingId,
   ) cancelCollectionDownload;
-  Future<LibraryItemEntity?> Function(String id) getLibraryItem;
 
   LibraryMethods({
-    required this.addToLibrary,
-    required this.getLibrary,
-    required this.addToPlaylist,
-    required this.deleteLibraryItem,
-    required this.toggleFavorite,
+    required this.getLibraryItems,
+    required this.getLibraryItem,
+    required this.createPlaylist,
+    required this.addTracksToPlaylist,
+    required this.removeTracksFromPlaylist,
+    required this.updatePlaylist,
+    required this.removePlaylistFromLibrary,
+    required this.addArtistToLibrary,
+    required this.removeArtistFromLibrary,
+    required this.addAlbumToLibrary,
+    required this.removeAlbumFromLibrary,
     required this.isFavorite,
     required this.loadFavorites,
+    required this.updateLastTimePlayed,
     required this.downloadCollection,
     required this.cancelCollectionDownload,
-    required this.getLibraryItem,
-    required this.updatePlaylistName,
-    required this.removeFromPlaylist,
-    required this.updateLastTimePlayed,
   });
 }
