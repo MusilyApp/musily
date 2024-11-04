c11475f55c55b444e59cd5b7edb97f6d5847ed1e
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-11-03 22:55:16 -0300

    added: all album options

--- lib/features/album/presenter/widgets/album_options_widget.dart
+++ lib/features/album/presenter/widgets/album_options_widget.dart
@@ -5,10 +5,12 @@ import 'package:musily/core/domain/entities/app_menu_entry.dart';
 import 'package:musily/core/domain/usecases/get_playable_item_usecase.dart';
 import 'package:musily/core/presenter/controllers/core/core_controller.dart';
 import 'package:musily/core/presenter/ui/buttons/ly_tonal_icon_button.dart';
+import 'package:musily/core/presenter/ui/utils/ly_navigator.dart';
 import 'package:musily/core/presenter/widgets/app_menu.dart';
 import 'package:musily/features/_library_module/presenter/controllers/library/library_controller.dart';
 import 'package:musily/features/album/domain/entities/album_entity.dart';
 import 'package:musily/features/album/domain/usecases/get_album_usecase.dart';
+import 'package:musily/features/album/presenter/widgets/album_static_tile.dart';
 import 'package:musily/features/artist/domain/usecases/get_artist_albums_usecase.dart';
 import 'package:musily/features/artist/domain/usecases/get_artist_singles_usecase.dart';
 import 'package:musily/features/artist/domain/usecases/get_artist_tracks_usecase.dart';
@@ -16,6 +18,7 @@ import 'package:musily/features/artist/domain/usecases/get_artist_usecase.dart';
 import 'package:musily/features/downloader/presenter/controllers/downloader/downloader_controller.dart';
 import 'package:musily/features/player/presenter/controllers/player/player_controller.dart';
 import 'package:musily/core/presenter/extensions/build_context.dart';
+import 'package:musily/features/playlist/presenter/widgets/playlist_adder.dart';
 
 class AlbumOptions extends StatelessWidget {
   final AlbumEntity album;
@@ -56,134 +59,191 @@ class AlbumOptions extends StatelessWidget {
       return downloaderController.builder(builder: (context, downloaderData) {
         final isAlbumDownloading = downloaderData.queue.isNotEmpty &&
             downloaderData.downloadingKey == album.id;
-        return AppMenu(
-          entries: [
-            AppMenuEntry(
-              onTap: () {
-                if (isAlbumDownloading) {
-                  libraryController.methods.cancelCollectionDownload(
-                    album.tracks,
-                    album.id,
-                  );
-                } else {
-                  libraryController.methods.downloadCollection(
-                    album.tracks,
-                    album.id,
-                  );
-                }
-              },
-              leading: Icon(
-                isAlbumDownloading
-                    ? Icons.cancel_rounded
-                    : Icons.download_rounded,
-                color: context.themeData.buttonTheme.colorScheme?.primary,
-              ),
-              title: Text(
-                isAlbumDownloading
-                    ? context.localization.cancelDownload
-                    : context.localization.download,
-              ),
-            ),
-            AppMenuEntry(
-              onTap: () async {
-                if (isAlbumPlaying) {
-                  if (playerData.isPlaying) {
-                    await playerController.methods.pause();
+        return libraryController.builder(builder: (context, libraryData) {
+          final isAlbumInLibrary = libraryData.items
+              .where((e) => e.album?.id == album.id)
+              .isNotEmpty;
+          return AppMenu(
+            modalHeader: AlbumStaticTile(album: album),
+            entries: [
+              AppMenuEntry(
+                onTap: () {
+                  if (isAlbumDownloading) {
+                    libraryController.methods.cancelCollectionDownload(
+                      album.tracks,
+                      album.id,
+                    );
                   } else {
-                    await playerController.methods.resume();
+                    libraryController.methods.downloadCollection(
+                      album.tracks,
+                      album.id,
+                    );
                   }
-                } else {
-                  await playerController.methods.playPlaylist(
-                    [
-                      ...album.tracks,
-                    ],
-                    album.id,
-                    startFrom: 0,
+                },
+                leading: Icon(
+                  isAlbumDownloading
+                      ? Icons.cancel_rounded
+                      : Icons.download_rounded,
+                  color: context.themeData.buttonTheme.colorScheme?.primary,
+                ),
+                title: Text(
+                  isAlbumDownloading
+                      ? context.localization.cancelDownload
+                      : context.localization.download,
+                ),
+              ),
+              AppMenuEntry(
+                onTap: () async {
+                  if (isAlbumPlaying) {
+                    if (playerData.isPlaying) {
+                      await playerController.methods.pause();
+                    } else {
+                      await playerController.methods.resume();
+                    }
+                  } else {
+                    await playerController.methods.playPlaylist(
+                      [
+                        ...album.tracks,
+                      ],
+                      album.id,
+                      startFrom: 0,
+                    );
+                    libraryController.methods.updateLastTimePlayed(
+                      album.id,
+                    );
+                  }
+                },
+                leading: Icon(
+                  isAlbumPlaying && playerData.isPlaying
+                      ? Icons.pause_rounded
+                      : Icons.play_arrow_rounded,
+                  color: context.themeData.buttonTheme.colorScheme?.primary,
+                ),
+                title: Text(
+                  isAlbumPlaying && playerData.isPlaying
+                      ? context.localization.pause
+                      : context.localization.play,
+                ),
+              ),
+              AppMenuEntry(
+                onTap: () async {
+                  final random = Random();
+                  final randomIndex = random.nextInt(
+                    album.tracks.length,
                   );
+                  playerController.methods.playPlaylist(
+                    album.tracks,
+                    album.id,
+                    startFrom: randomIndex,
+                  );
+                  if (!playerData.shuffleEnabled) {
+                    playerController.methods.toggleShuffle();
+                  } else {
+                    await playerController.methods.toggleShuffle();
+                    playerController.methods.toggleShuffle();
+                  }
                   libraryController.methods.updateLastTimePlayed(
                     album.id,
                   );
-                }
-              },
-              leading: Icon(
-                isAlbumPlaying && playerData.isPlaying
-                    ? Icons.pause_rounded
-                    : Icons.play_arrow_rounded,
-                color: context.themeData.buttonTheme.colorScheme?.primary,
-              ),
-              title: Text(
-                isAlbumPlaying && playerData.isPlaying
-                    ? context.localization.pause
-                    : context.localization.play,
-              ),
-            ),
-            AppMenuEntry(
-              onTap: () async {
-                final random = Random();
-                final randomIndex = random.nextInt(
-                  album.tracks.length,
-                );
-                playerController.methods.playPlaylist(
-                  album.tracks,
-                  album.id,
-                  startFrom: randomIndex,
-                );
-                if (!playerData.shuffleEnabled) {
-                  playerController.methods.toggleShuffle();
-                } else {
-                  await playerController.methods.toggleShuffle();
-                  playerController.methods.toggleShuffle();
-                }
-                libraryController.methods.updateLastTimePlayed(
-                  album.id,
-                );
-              },
-              leading: Icon(
-                Icons.shuffle_rounded,
-                color: context.themeData.buttonTheme.colorScheme?.primary,
-              ),
-              title: Text(
-                context.localization.shufflePlay,
-              ),
-            ),
-            AppMenuEntry(
-              onTap: () {
-                playerController.methods.addToQueue(
-                  album.tracks,
-                );
-              },
-              leading: Icon(
-                Icons.playlist_add,
-                color: context.themeData.buttonTheme.colorScheme?.primary,
-              ),
-              title: Text(
-                context.localization.addToQueue,
-                style: const TextStyle(
-                  color: null,
+                },
+                leading: Icon(
+                  Icons.shuffle_rounded,
+                  color: context.themeData.buttonTheme.colorScheme?.primary,
+                ),
+                title: Text(
+                  context.localization.shufflePlay,
                 ),
               ),
-            ),
-          ],
-          coreController: coreController,
-          toggler: (context, invoke) {
-            if (tonal) {
-              return LyTonalIconButton(
+              AppMenuEntry(
+                onTap: () {
+                  playerController.methods.addToQueue(
+                    album.tracks,
+                  );
+                },
+                leading: Icon(
+                  Icons.playlist_add,
+                  color: context.themeData.buttonTheme.colorScheme?.primary,
+                ),
+                title: Text(
+                  context.localization.addToQueue,
+                  style: const TextStyle(
+                    color: null,
+                  ),
+                ),
+              ),
+              AppMenuEntry(
+                leading: Icon(
+                  Icons.queue_music,
+                  color: context.themeData.buttonTheme.colorScheme?.primary,
+                ),
+                title: Text(
+                  context.localization.addToPlaylist,
+                ),
+                onTap: () {
+                  LyNavigator.push(
+                    coreController.coreContext!,
+                    PlaylistAdderWidget(
+                      coreController: coreController,
+                      libraryController: libraryController,
+                      tracks: album.tracks,
+                    ),
+                  );
+                },
+              ),
+              AppMenuEntry(
+                onTap: () async {
+                  if (isAlbumInLibrary) {
+                    await libraryController.methods.removeAlbumFromLibrary(
+                      album.id,
+                    );
+                  } else {
+                    await libraryController.methods.addAlbumToLibrary(
+                      album,
+                    );
+                  }
+                },
+                leading: Icon(
+                  isAlbumInLibrary
+                      ? Icons.delete_rounded
+                      : Icons.library_add_rounded,
+                  color: context.themeData.buttonTheme.colorScheme?.primary,
+                ),
+                title: Text(
+                  isAlbumInLibrary
+                      ? context.localization.removeFromLibrary
+                      : context.localization.addToLibrary,
+                ),
+              ),
+              AppMenuEntry(
+                onTap: () {},
+                leading: Icon(
+                  Icons.share_rounded,
+                  color: Theme.of(context).buttonTheme.colorScheme?.primary,
+                ),
+                title: Text(context.localization.share),
+              ),
+            ],
+            coreController: coreController,
+            toggler: (context, invoke) {
+              if (tonal) {
+                return LyTonalIconButton(
+                  onPressed: invoke,
+                  fixedSize: const Size(55, 55),
+                  onFocus: onFocus,
+                  icon: const Icon(
+                    Icons.more_vert,
+                  ),
+                );
+              }
+              return IconButton(
                 onPressed: invoke,
-                fixedSize: const Size(55, 55),
-                onFocus: onFocus,
                 icon: const Icon(
                   Icons.more_vert,
                 ),
               );
-            }
-            return IconButton(
-              onPressed: invoke,
-              icon: const Icon(
-                Icons.more_vert,
-              ),
-            );
-          },
-        );
+            },
+          );
+        });
       });
     });
   }
---
+++ lib/features/album/presenter/widgets/album_static_tile.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:musily/core/presenter/extensions/build_context.dart';
+import 'package:musily/core/presenter/ui/boxes/ly_card.dart';
+import 'package:musily/core/presenter/ui/lists/ly_list_tile.dart';
+import 'package:musily/core/presenter/widgets/app_image.dart';
+import 'package:musily/core/presenter/widgets/infinity_marquee.dart';
+import 'package:musily/features/album/domain/entities/album_entity.dart';
+
+class AlbumStaticTile extends StatelessWidget {
+  final AlbumEntity album;
+  const AlbumStaticTile({
+    super.key,
+    required this.album,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LyListTile(
+      leading: LyCard(
+        width: 40,
+        borderRadius: BorderRadius.circular(8),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+          side: BorderSide(
+            width: 1,
+            color: context.themeData.colorScheme.outline.withOpacity(.2),
+          ),
+        ),
+        padding: EdgeInsets.zero,
+        content: ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: AppImage(
+            album.lowResImg ?? '',
+            width: 40,
+          ),
+        ),
+      ),
+      title: InfinityMarquee(
+        child: Text(album.title),
+      ),
+      subtitle: InfinityMarquee(
+        child: Text(album.artist.name),
+      ),
+    );
+  }
+}
