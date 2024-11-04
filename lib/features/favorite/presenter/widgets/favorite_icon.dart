85b5c8019a2bf238818cf48a3e32cb0162d64a71
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-11-04 17:53:34 -0300

    added: animations

--- lib/features/favorite/presenter/widgets/favorite_icon.dart
+++ lib/features/favorite/presenter/widgets/favorite_icon.dart
@@ -1,20 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:musily/core/presenter/extensions/build_context.dart';
 
-class FavoriteIcon extends StatelessWidget {
+class FavoriteIcon extends StatefulWidget {
   final double size;
   final double? iconSize;
+  final bool animated;
+
   const FavoriteIcon({
     super.key,
     this.size = 50,
     this.iconSize,
+    this.animated = false,
   });
+
+  @override
+  State<FavoriteIcon> createState() => _FavoriteIconState();
+}
+
+class _FavoriteIconState extends State<FavoriteIcon>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _scaleAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(milliseconds: 600),
+      vsync: this,
+    );
+
+    _scaleAnimation = Tween<double>(begin: 1.0, end: 1.05).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOutCubic,
+      ),
+    );
+
+    if (widget.animated) {
+      _controller.repeat(reverse: true);
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant FavoriteIcon oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.animated != oldWidget.animated) {
+      widget.animated ? _controller.repeat(reverse: true) : _controller.stop();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: size,
-      height: size,
+      width: widget.size,
+      height: widget.size,
       decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topLeft,
@@ -26,10 +72,15 @@ class FavoriteIcon extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Icon(
-          Icons.favorite_rounded,
-          color: Colors.white,
-          size: iconSize,
+        child: ScaleTransition(
+          scale: widget.animated
+              ? _scaleAnimation
+              : const AlwaysStoppedAnimation(1.0),
+          child: Icon(
+            Icons.favorite_rounded,
+            color: Colors.white,
+            size: widget.iconSize ?? widget.size * 0.6,
+          ),
         ),
       ),
     );
--- lib/features/player/presenter/widgets/player_background.dart
+++ lib/features/player/presenter/widgets/player_background.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+import 'dart:math';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:musily/core/presenter/extensions/build_context.dart';
 import 'package:musily/core/presenter/widgets/app_image.dart';
 import 'package:musily/features/player/presenter/controllers/player/player_controller.dart';
@@ -20,19 +23,66 @@ class PlayerBackground extends StatefulWidget {
 }
 
 class _PlayerBackgroundState extends State<PlayerBackground> {
+  int selectedAnimatedWidget = 0;
+  bool changingAnimatedWidget = false;
+  Timer? _timer;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedAnimatedWidget = Random().nextInt(4);
+    _startAnimationTimer();
+  }
+
+  void _startAnimationTimer() {
+    _timer = Timer.periodic(const Duration(seconds: 10), (timer) {
+      setState(() {
+        changingAnimatedWidget = true;
+      });
+      Future.delayed(const Duration(milliseconds: 400), () {
+        setState(() {
+          selectedAnimatedWidget = Random().nextInt(4);
+          changingAnimatedWidget = false;
+        });
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  Widget _getAnimatedWidget(BuildContext context) {
+    final items = [
+      LoadingAnimationWidget.waveDots(
+        color: context.themeData.dividerColor,
+        size: context.display.width,
+      ),
+      LoadingAnimationWidget.staggeredDotsWave(
+        color: context.themeData.dividerColor,
+        size: context.display.width,
+      ),
+      LoadingAnimationWidget.progressiveDots(
+        color: context.themeData.dividerColor,
+        size: context.display.width,
+      ),
+      LoadingAnimationWidget.horizontalRotatingDots(
+        color: context.themeData.dividerColor,
+        size: context.display.width,
+      ),
+      LoadingAnimationWidget.stretchedDots(
+        color: context.themeData.dividerColor,
+        size: context.display.width,
+      ),
+    ];
+    return items[selectedAnimatedWidget];
+  }
+
   @override
   Widget build(BuildContext context) {
-    final brightness = MediaQuery.of(context).platformBrightness;
-    final maxWidth = MediaQuery.of(context).size.width;
-    final maxHeight = MediaQuery.of(context).size.height;
-
     return widget.playerController.builder(builder: (context, data) {
-      bool isDarkMode = () {
-        if (data.themeMode == null || data.themeMode == ThemeMode.system) {
-          return brightness == Brightness.dark;
-        }
-        return data.themeMode == ThemeMode.dark;
-      }();
       return Stack(
         children: [
           AnimatedOpacity(
@@ -46,8 +96,8 @@ class _PlayerBackgroundState extends State<PlayerBackground> {
             opacity: data.showLyrics ? 0.0 : 1.0,
             duration: const Duration(milliseconds: 200),
             child: SizedBox(
-              width: maxWidth,
-              height: maxHeight,
+              width: context.display.width,
+              height: context.display.height,
               child: ImageFiltered(
                 imageFilter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
                 child: AppImage(
@@ -56,8 +106,36 @@ class _PlayerBackgroundState extends State<PlayerBackground> {
                   color: context.themeData.scaffoldBackgroundColor
                       .withOpacity(0.9),
                   colorBlendMode:
-                      isDarkMode ? BlendMode.darken : BlendMode.lighten,
+                      context.isDarkMode ? BlendMode.darken : BlendMode.lighten,
                 ),
+              ),
+            ),
+          ),
+          AnimatedOpacity(
+            opacity:
+                data.isPlaying && !data.showLyrics && !changingAnimatedWidget
+                    ? 1
+                    : 0,
+            duration: const Duration(milliseconds: 400),
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+              child: Align(
+                alignment: Alignment.topCenter,
+                child: _getAnimatedWidget(context),
+              ),
+            ),
+          ),
+          AnimatedOpacity(
+            opacity:
+                data.isPlaying && !data.showLyrics && !changingAnimatedWidget
+                    ? 1
+                    : 0,
+            duration: const Duration(milliseconds: 400),
+            child: ImageFiltered(
+              imageFilter: ImageFilter.blur(sigmaX: 50, sigmaY: 50),
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: _getAnimatedWidget(context),
               ),
             ),
           ),
