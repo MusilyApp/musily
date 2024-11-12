d1b738d899c66e038649c441089b52477c264110
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-11-12 01:04:47 -0300

    added: error handling

--- lib/core/data/usecases/get_playable_item_usecase_impl.dart
+++ lib/core/data/usecases/get_playable_item_usecase_impl.dart
@@ -1,6 +1,7 @@
 import 'package:musily/core/presenter/extensions/string.dart';
 import 'package:musily/core/domain/repositories/musily_repository.dart';
 import 'package:musily/core/domain/usecases/get_playable_item_usecase.dart';
+import 'package:musily/core/presenter/ui/utils/ly_error_handler.dart';
 import 'package:musily/features/track/domain/entities/track_entity.dart';
 import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
@@ -25,29 +26,34 @@ class GetPlayableItemUsecaseImpl implements GetPlayableItemUsecase {
       ytId = track.id;
     }
 
-    final manifest = await yt.videos.streamsClient.getManifest(VideoId(ytId));
-    final audioSteamInfo = manifest.audioOnly.withHighestBitrate();
+    try {
+      final manifest = await yt.videos.streamsClient.getManifest(VideoId(ytId));
+      final audioSteamInfo = manifest.audioOnly.withHighestBitrate();
 
-    url = audioSteamInfo.url.toString();
+      url = audioSteamInfo.url.toString();
 
-    if (!(track.lowResImg?.isUrl ?? false) ||
-        !(track.highResImg?.isUrl ?? false)) {
-      final updatedTrack = await _musilyRepository.getTrack(track.id);
-      track.highResImg = updatedTrack?.highResImg;
-      track.lowResImg = updatedTrack?.lowResImg;
+      if (!(track.lowResImg?.isUrl ?? false) ||
+          !(track.highResImg?.isUrl ?? false)) {
+        final updatedTrack = await _musilyRepository.getTrack(track.id);
+        track.highResImg = updatedTrack?.highResImg;
+        track.lowResImg = updatedTrack?.lowResImg;
+      }
+
+      return TrackEntity(
+        id: track.id,
+        hash: track.hash,
+        title: track.title,
+        artist: track.artist,
+        highResImg: track.highResImg,
+        lowResImg: track.lowResImg,
+        url: url.isUrl ? url : null,
+        album: track.album,
+        fromSmartQueue: track.fromSmartQueue,
+        duration: track.duration,
+      );
+    } catch (e) {
+      LyErrorHandler.snackBar(e.toString());
+      return track;
     }
-
-    return TrackEntity(
-      id: track.id,
-      hash: track.hash,
-      title: track.title,
-      artist: track.artist,
-      highResImg: track.highResImg,
-      lowResImg: track.lowResImg,
-      url: url.isUrl ? url : null,
-      album: track.album,
-      fromSmartQueue: track.fromSmartQueue,
-      duration: track.duration,
-    );
   }
 }
