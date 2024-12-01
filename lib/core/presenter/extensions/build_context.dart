1a2764ccbc0305408673840bae4ac79c65274319
Felipe Yslaoker <felipeyslaok@gmail.com>
2024-12-01 18:48:37 -0300

    added: new method extensions

--- lib/core/presenter/extensions/build_context.dart
+++ lib/core/presenter/extensions/build_context.dart
@@ -56,4 +56,6 @@ extension MusilyBuildContext on BuildContext {
   ThemeData get themeData {
     return Theme.of(this);
   }
+
+  MediaQueryData get mediaQuery => MediaQuery.of(this);
 }
--- lib/core/presenter/extensions/date_time.dart
+++ lib/core/presenter/extensions/date_time.dart
@@ -6,4 +6,15 @@ extension LyDateTime on DateTime {
 
     return '$hours:$minutes:$seconds';
   }
+
+  String toFormattedDate() {
+    String year = this.year.toString();
+    String month = this.month.toString().padLeft(2, '0');
+    String day = this.day.toString().padLeft(2, '0');
+    String hour = this.hour.toString().padLeft(2, '0');
+    String minutes = minute.toString().padLeft(2, '0');
+    String seconds = second.toString().padLeft(2, '0');
+
+    return '$year-$month-$day-$hour:$minutes:$seconds';
+  }
 }
