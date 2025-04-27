7071b66eb1876d7a54094d805ef61bfe8c140e78
FelipeYslaoker <felipeyslaok@gmail.com>
2025-04-27 16:56:19 -0300

    improved app theme

--- lib/core/presenter/widgets/musily_logo.dart
+++ lib/core/presenter/widgets/musily_logo.dart
@@ -18,12 +18,45 @@ class MusilyLogo extends StatefulWidget {
 class _MusilyLogoState extends State<MusilyLogo> {
   @override
   Widget build(BuildContext context) {
-    return context.settingsBuilder(
-      builder: (context, data) => SvgPicture.asset(
-        'assets/icons/musily_appbar_icon${context.isDarkMode ? '_dark' : ''}.svg',
+    return context.settingsBuilder(builder: (context, data) {
+      late final Color defaultColor;
+      if (context.isDarkMode) {
+        defaultColor = const Color(0xFFe8def7);
+      } else {
+        defaultColor = const Color(0xFF68548f);
+      }
+      return SvgPicture.asset(
+        'assets/icons/musily_appbar_icon.svg',
+        colorFilter: ColorFilter.mode(
+            context.themeData.buttonTheme.colorScheme?.primary ?? defaultColor,
+            BlendMode.srcATop),
         width: widget.width,
         height: widget.height,
-      ),
-    );
+      );
+    });
+  }
+}
+
+class MusilyTitle extends StatelessWidget {
+  const MusilyTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return context.settingsBuilder(builder: (context, data) {
+      late final Color defaultColor;
+      if (context.isDarkMode) {
+        defaultColor = const Color(0xFFe8def7);
+      } else {
+        defaultColor = const Color(0xFF68548f);
+      }
+      return Text(
+        'Musily',
+        style: context.themeData.textTheme.titleLarge?.copyWith(
+          fontWeight: FontWeight.w400,
+          color: context.themeData.buttonTheme.colorScheme?.primary ??
+              defaultColor,
+        ),
+      );
+    });
   }
 }
