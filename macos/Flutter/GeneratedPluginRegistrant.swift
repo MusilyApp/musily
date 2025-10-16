5b191448d4cdcfd517f28d9b2a3fa940c8ddc5f3
Felipe Yslaoker <felipeyslaok@gmail.com>
2025-10-16 11:44:27 -0300

    chore: update sqflite import to sqflite_darwin for macOS compatibility

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -21,7 +21,7 @@ import path_provider_foundation
 import screen_retriever
 import share_plus
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import tray_manager
 import url_launcher_macos
 import window_manager
