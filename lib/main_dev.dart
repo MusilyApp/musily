9e2e9844dce969f4c32fd7dd9c8963b9040f3889
Felipe Yslaoker <felipeyslaok@gmail.com>
2025-11-14 03:51:48 -0300

    refactor: remove unused IPC service imports and initialization logic for cleaner main function

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -6,9 +6,6 @@ import 'package:flutter_modular/flutter_modular.dart';
 import 'package:media_store_plus/media_store_plus.dart';
 import 'package:musily/core/data/database/database.dart';
 import 'package:musily/core/data/repositories/musily_repository_impl.dart';
-import 'package:musily/core/data/services/ipc_service.dart';
-import 'package:musily/core/data/services/ipc_service_unix.dart';
-import 'package:musily/core/data/services/ipc_service_windows.dart';
 import 'package:musily/core/data/services/library_migration.dart';
 import 'package:musily/core/data/services/tray_service.dart';
 import 'package:musily/core/data/services/updater_service.dart';
@@ -17,25 +14,11 @@ import 'package:musily/features/player/data/services/musily_service.dart';
 import 'package:musily/core/data/services/user_service.dart';
 import 'package:musily/core/presenter/widgets/app_material.dart';
 import 'package:musily/core/core_module.dart';
-import 'package:smtc_windows/smtc_windows.dart';
 
 final mediaStorePlugin = MediaStore();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
-    late IPCService ipcService;
-    if (Platform.isWindows) {
-      ipcService = IPCServiceWindows();
-    } else {
-      ipcService = IPCServiceUnix();
-    }
-    final isFirstInstance = await ipcService.initializeIpcServer();
-    if (!isFirstInstance) {
-      exit(0);
-    }
-  }
 
   await Database().init();
 
@@ -48,10 +31,6 @@ void main() async {
   if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     await WindowService.init();
     await TrayService.init();
-  }
-
-  if (Platform.isWindows) {
-    await SMTCWindows.initialize();
   }
 
   final userService = UserService();
